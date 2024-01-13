69ec3dc5b676dc0b03960bb64d571cdd2a253547
syrene bartolome <syrene.h.bartolome@icloud.com>
2024-01-13 12:33:49 +0900

    completed lists routes, controller, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create]
 end
